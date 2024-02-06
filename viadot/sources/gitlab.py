a6f17d891cb803302baf5a0b6e25f703f08ee2c5
judynah <mrs.nataliawalczak@gmail.com>
2024-02-06 15:45:36 +0100

    :art: added get_conn function

--- viadot/sources/gitlab.py
+++ viadot/sources/gitlab.py
@@ -1,28 +1,28 @@
 from typing import Any, Dict, Literal
+import gitlab
 
-from .base import Source
-from ..config import local_config
-from ..exceptions import CredentialError
+from viadot.sources.base import Source
+from viadot.config import local_config
+from viadot.exceptions import CredentialError
 
 
 class Gitlab(Source):
-
     """
     Source for getting data from Gitlab API.
 
     """
+
     def __init__(
         self,
         config_key: str = "Gitlab",
         credentials: Dict[str, Any] = None,
         url: str = None,
-        *args, 
+        *args,
         **kwargs
-        ):
+    ):
         """
-            Creating an instance of Gitlab source class.
+        Creating an instance of Gitlab source class.
         """
-
 
         if config_key:
             config_credentials = local_config.get(config_key)
@@ -30,13 +30,13 @@ class Gitlab(Source):
         credentials = credentials if credentials else config_credentials
         if credentials is None:
             raise CredentialError("Credentials not found.")
-        
+
         self.config_key = config_key
         self.url = url
 
         super().__init__(*args, credentials=credentials, **kwargs)
 
-
-
-    # def get_data(self):
-    #     response = 
+    def get_conn(self, url):
+        return gitlab.Gitlab(
+            url=self.url, private_token=self.credentials.get("private_token")
+        )
