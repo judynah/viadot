0ed19814d82f328ffca5726ca98abaf202681c5b
judynah <mrs.nataliawalczak@gmail.com>
2024-03-27 16:51:05 +0100

    :zap: added GetWiki task

--- viadot/sources/gitlab.py
+++ viadot/sources/gitlab.py
@@ -1,6 +1,5 @@
 from typing import Any, Dict, Literal
 import gitlab
-
 from viadot.sources.base import Source
 from viadot.config import local_config
 from viadot.exceptions import CredentialError
@@ -9,37 +8,48 @@ from viadot.exceptions import CredentialError
 class Gitlab(Source):
 
     """
-    Source for getting data from Gitlab API.
+    Source for retrieving and updateing data using the Gitlab API.
     
     """
     def __init__(
         self,
+        url: str,
         config_key: str = "Gitlab",
         credentials: Dict[str, Any] = None,
-        url: str = None,
         *args, 
         **kwargs
         ):
-        """
+        """a
             Creating an instance of Gitlab source class.
+            
+            Args:
+                config_key (str, optional): Credential key to dictionary where credentials are stored. Defaults to "Gitlab".
+                credentials (Dict[str, Any], optional): Credentials stored in a dictionary. Defaults to None.
+                url (str, required): URL adress to repository in the  `https://gitlab.(organization).com/repo`. Defaults to None.
+                
+            Raises:
+                CredentialError: If provided credentials are missing.
+                ValueError: If url are missing or has not appropriate form.
         """
+        
+        DEFAULT_CREDENTIALS = local_config.get(config_key)
+        self.credentials = credentials or DEFAULT_CREDENTIALS
 
-        if config_key:
-            config_credentials = local_config.get(config_key)
-
-        self.credentials = credentials if credentials else config_credentials
         if self.credentials is None:
             raise CredentialError("Credentials not found.")
-        
-        self.config_key = config_key
+ 
         self.url = url
-        # self.con = None
+        if self.url is None:
+            raise ValueError("URL is required.")
 
         super().__init__(*args, credentials=self.credentials, **kwargs)
-
+        
+    """
+        Creating the connection to Gitlab.
+    """
     def get_conn(self):
         return gitlab.Gitlab(
             url = self.url,
-            private_token=self.credentials.get("private_token"))
+            private_token=self.credentials['token'])
         
        
--- viadot/tasks/gitlab.py
+++ viadot/tasks/gitlab.py
@@ -6,21 +6,39 @@ from gitlab.exceptions import GitlabHttpError,GitlabGetError
 from prefect.utilities.tasks import defaults_from_attrs
 
 
+
+
 class UpdateWiki(Task):
     def __init__(
         self,
-        url: str = None,
-        access_token_secret: str = None,
-        branch: str = None,
+        url: str,
+        credentials: str = None,
+        config_key: str =  "Gitlab",
         timeout: int = 3600,
         *args,
         **kwargs
     ):
+        
+        """Task for updating Gitlab WikiPage.
+        
+        Args:
+            url (str, required): URL adress to repository in the Gitlab in form  `https://gitlab.(organization).com/repo`. Defaults to None.
+            credentials (Dict[str, Any], optional): Credentials stored in a dictionary. Required credentials: token. Defaults to None.
+            config_key (str, optional): Credential key to dictionary where details are stored. Defaults to None.
+            timeout (int, optional): The amount of time (in seconds) to wait while running this task before a timeout occurs. Defaults to 3600.
+            
+        Returns:
+            ValueError: If provided url not found.
 
+        """
+        
         self.url = url
-        self.branch = branch
-        self.access_token_secret=access_token_secret
-
+        if self.url is None:
+            raise ValidationError("No valid url found.")
+        
+        self.config_key = config_key
+        self.credentials = credentials
+        
         super().__init__(
             name="update_gitlab_data", 
             timeout=timeout, 
@@ -29,38 +47,45 @@ class UpdateWiki(Task):
 
     def run(
             self, 
-            url: str = None,
-            wiki_path: str = None,
-            wiki_content: str = None,
+            wiki_path: str,
+            wiki_content: str = "",
             file_path: str = None,
-            branch: str = None,
             timeout: int = 3600,
-    ):
+    ) -> dict:
+        """Task run method.
+        
+            Args:
+                wiki_path (str, required): Path to wiki page that derived from url to the wiki page after `wikis` ex. `/home/Entities/Automatio/data/view`.
+                wiki_content (str, optional): New content of wiki page. Default to None.
+                file_path (str, optional): Path to the file, that has to be uploaded to the Wiki Page. Default to None.
+                timeout (int, optional): The amount of time (in seconds) to wait while running this task before a timeout occurs. Defaults to 3600.
+                
+            Returns:
+                Dict: Returns a dictonary with page attributes.
+        """
         
         gitlab_url = re.search(".*gitlab\.[^/]*/", self.url).group()
         project_namespace = self.url.replace(gitlab_url, "")
 
-        gitlab = Gitlab(url = gitlab_url, credentials=self.access_token_secret)
+        gitlab = Gitlab(url = gitlab_url, credentials=self.credentials, config_key=self.config_key)
         gl = gitlab.get_conn()
         # gitlab.auth()
-
-        project = gl.projects.get(project_namespace)
         
-        # if wiki_path is None:
-        #     raise NameError("Wiki page not found.")
+        project = gl.projects.get(project_namespace)
+        page = project.wikis.get(wiki_path)
+
+        
+        # try:
+        #     page = project.wikis.get(wiki_path)
+        # except (GitlabHttpError, GitlabGetError):
+        #     print("Wiki Page does not exist")
+            # page = project.wikis.create({'title': wiki_path, 
+            #                             'content': None})
             
-        try:
-            page = project.wikis.get(wiki_path)
-        except (GitlabHttpError, GitlabGetError, NameError):
-            print("Wrong wiki page path. Create new wiki page")
-            page = project.wikis.create({'title': wiki_path, 
-                                        'content': None})
-            
-    
-            
+
         page.content = wiki_content
         if file_path is not None:
-            temp = page.upload(file_name = file_name.split("/")[-1], file_path=file_path)
+            temp = page.upload(filename = file_path.split("/")[-1], filepath=file_path)
             wiki_content = page.content + '\n\n'+ temp['link']['markdown']
         page.content = wiki_content
         page.title = page.slug
@@ -73,17 +98,30 @@ class UpdateWiki(Task):
 class GetWiki(Task):
     def __init__(
         self,
-        url: str = None,
-        access_token_secret: str = None,
-        branch: str = None,
+        url: str,
+        credentials: str = None,
+        config_key: str = "Gitlab",
         timeout: int = 3600,
         *args,
         **kwargs
     ):
+        """Task for getting Gitlab WikiPage.
+        
+            Args:
+                url (str, required): URL adress to repository in the Gitlab in form  `https://gitlab.(organization).com/repo`. Defaults to None.
+                credentials (Dict[str, Any], optional): Credentials stored in a dictionary. Required credentials: token. Defaults to None.
+                config_key (str, optional): Credential key to dictionary where details are stored. Defaults to None.
+                timeout (int, optional): The amount of time (in seconds) to wait while running this task before a timeout occurs. Defaults to 3600.
+                
+            Returns:
+                Str: Returns content of wiki page in string format.
+       
+        """
+        
 
         self.url = url
-        self.branch = branch
-        self.access_token_secret=access_token_secret
+        self.config_key = config_key
+        self.credentials = credentials
 
         super().__init__(
             name="get_wiki", 
@@ -97,32 +135,47 @@ class GetWiki(Task):
     # )
     def run(
             self, 
-            url: str = None,
-            wiki_path: str = None,
-            branch: str = None,
+            wiki_path: str,
+            wiki_list: bool = False,
             timeout: int = 3600,
     ) -> str:
+        
+        """Task run method.
+        
+            Args:
+                wiki_path (str, required): Path to wiki page that derived from url to the wiki page after `wikis` ex. `/home/Entities/Automatio/data/view`.
+                wiki_list (bool, optional): Indicator if list of wiki page has to be received. Default to False.
+                timeout (int, optional): The amount of time (in seconds) to wait while running this task before a timeout occurs. Defaults to 3600.
+                
+            Returns:
+                Str: Content of wiki page.
+
+        """
+        
+        self.wiki_path=wiki_path
+        self.wiki_list = wiki_list
+        self.wiki_sidebar = wiki_sidebar
+        
+        if self.url==None:
+            raise ValueError("URL are required")
+            
+        if self.wiki_path==None:
+            raise ValueError("Wiki path are required")
         
         gitlab_url = re.search(".*gitlab\.[^/]*/", self.url).group()
         project_namespace = self.url.replace(gitlab_url, "")
 
-        gitlab = Gitlab(url = gitlab_url, credentials=self.access_token_secret)
+        gitlab = Gitlab(url = gitlab_url, credentials=self.credentials, config_key=self.config_key)
         gl = gitlab.get_conn()
         # gitlab.auth()
-
+        
         project = gl.projects.get(project_namespace)
         
-        # if wiki_path is None:
-        #     raise NameError("Wiki page not found.")
+        if self.wiki_list==True:
+            return project.wikis.list()
             
-        try:
-            page = project.wikis.get(wiki_path)
-        except (GitlabHttpError, GitlabGetError, NameError):
-            print("Wrong wiki page path. Create new wiki page")
-            # page = project.wikis.create({'title': wiki_path, 
-                                        # 'content': None})
+
+        page = project.wikis.get(wiki_path)
 
         return page.attributes['content']
             
-        
-
